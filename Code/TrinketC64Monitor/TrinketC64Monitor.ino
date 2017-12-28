e0931b598f848db32ef141f94219d0d631fe0ea8
Leif Bloomquist <github@leifbloomquist.net>
2017-12-28 00:22:15 -0500

    Cylon with timers

--- Code/TrinketC64Monitor/TrinketC64Monitor.ino
+++ Code/TrinketC64Monitor/TrinketC64Monitor.ino
@@ -1,4 +1,5 @@
 #include <Adafruit_NeoPixel.h>
+#include <SimpleTimer.h>
 
 // Proto board Wiring
 // RED         = 5V
@@ -10,22 +11,12 @@
 #endif
 
 #define NUM_PIXELS 8
+#define PIN_PIXELS 0   // 0 for Trinket,
 
-#define PIN_PIXELS 0   // 0 for Trinket, 13 for Nano
-
-// Parameter 1 = number of pixels in strip
-// Parameter 2 = Arduino pin number (most are valid)
-// Parameter 3 = pixel type flags, add together as needed:
-//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
-//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
-//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
-//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_PIXELS, PIN_PIXELS, NEO_GRB + NEO_KHZ800);
 
-// IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
-// pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
-// and minimize distance between Arduino and first pixel.  Avoid connecting
-// on a live circuit...if you must, connect GND first.
+// The timer object
+SimpleTimer timer;
 
 void setup() {
     // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
@@ -33,6 +24,8 @@ void setup() {
     if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
 #endif
     // End of trinket special code
+
+    timer.setInterval(100, Animate);
 
     strip.begin();
     strip.show(); // Initialize all pixels to 'off'   
@@ -44,18 +37,27 @@ float bright[NUM_PIXELS] = { 0 };
 
 void loop()
 {
-    pixel += pixeldir;
+    timer.run();
+}
 
-    if ((pixel >= NUM_PIXELS) || (pixel < 0))
+void Animate()
+{
+    while (true)
     {
-        pixeldir = -pixeldir;
         pixel += pixeldir;
-        return;  // to start of loop()
+
+        if ((pixel >= NUM_PIXELS) || (pixel < 0))
+        {
+            pixeldir = -pixeldir;
+            pixel += pixeldir;
+            pixel += pixeldir;
+            continue;  // to start of while()
+        }
+
+        bright[pixel] = 1.0;
+        break;
     }
 
-    bright[pixel] = 1.0;
-
-    delay(100);
     Refresh();
 }
 
